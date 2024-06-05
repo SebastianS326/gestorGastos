ba148fa8914076a890e026f5e6b690ed547125f6
SebastianS326 <ss3016373002@gmail.com>
2024-06-04 22:47:31 -0500

    ss

---
+++ src/components/control_presupuesto.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <form>
+        <section class="campo">
+            <label for="">Definir Presupuesto</label>
+            <input id="nuevo-presupuesto" class="nuevo-presupuesto" placeholder="Agrega tu Presupuesto">
+        </section>
+
+        <input type="submit" value="Definir Presupuesto">
+    </form>
+</template>
+
+
+<style scoped>
+
+</style>
