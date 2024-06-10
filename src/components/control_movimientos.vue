88b5cfe4c8daec4c5e11098635a70e3c83d9ce7c
SebastianS326 <ss3016373002@gmail.com>
2024-06-10 15:33:09 -0500

    Documentacion 3

--- src/components/control_movimientos.vue
+++ src/components/control_movimientos.vue
@@ -1,40 +1,50 @@
-<script setup>
-    import { defineProps } from 'vue'
-    import { formatoDinero, formatoFecha } from '../Funtions'
+<!-- control_movimientos.vue -->
 
-    const props = defineProps({
-        gasto: {
-            type: Object,
-            required: true
-        }
-    })
+<script setup>
+import { defineProps } from 'vue'
+import { formatoDinero, formatoFecha } from '../Funtions'
+
+// Definición de propiedades
+const props = defineProps({
+    // 'gasto': Objeto que contiene los detalles del gasto
+    gasto: {
+        type: Object,
+        required: true
+    }
+})
 </script>
 
 <template>
+    <!-- Contenedor principal del movimiento -->
     <div class="movimiento sombra">
+        <!-- Sección de contenido del movimiento -->
         <section class="contenido">
             <div>
+                <!-- Mostrar el nombre del gasto -->
                 <p> <span>Concepto: </span> {{ props.gasto.nombre }} </p>
+                <!-- Mostrar la categoría del gasto -->
                 <p> <span>Categoria: </span> {{ props.gasto.categoria }} </p>
+                <!-- Mostrar la fecha del gasto, formateada -->
                 <p> <span>Fecha: </span> {{ formatoFecha(props.gasto.fecha) }} </p>
+                <!-- Mostrar el valor del gasto, formateado -->
                 <p> <span>Valor: </span> {{ formatoDinero(props.gasto.cantidad) }} </p>
             </div>
-
         </section>
-        
     </div>
 </template>
 
 <style scoped>
-    .contenido p span {
-        font-size: 2rem;
-        font-weight: 900;
-        color: var(--GrisAzul-700);
-    }
+/* Estilo para los <span> dentro de los párrafos */
+.contenido p span {
+    font-size: 2rem; /* Tamaño de fuente de 2rem */
+    font-weight: 900; /* Peso de fuente 900 (negrita) */
+    color: var(--GrisAzul-700); /* Color gris oscuro */
+}
 
-    .contenido p {
-        font-size: 1.8rem;
-        font-weight: 500;
-        color: var(--GrisAzul-500);
-    }
+/* Estilo para los párrafos */
+.contenido p {
+    font-size: 1.8rem; /* Tamaño de fuente de 1.8rem */
+    font-weight: 500; /* Peso de fuente 500 (medio) */
+    color: var(--GrisAzul-500); /* Color gris medio */
+}
 </style>
