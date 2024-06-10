b462f6daed8970d54d6f0a3483e53cd0c81b064f
SebastianS326 <ss3016373002@gmail.com>
2024-06-10 14:08:09 -0500

    Documentacion 2

--- src/components/ventana_formulario.vue
+++ src/components/ventana_formulario.vue
@@ -1,12 +1,18 @@
+<!-- ventana_formulario.vue -->
+
 <script setup>
 import { defineEmits, defineProps, ref } from 'vue'
 
 import global_error from './gobal_error.vue'
 import iconoCerrar from '../assets/images/closeRed.svg'
 
+// Referencia reactiva para el mensaje de error
 const error = ref('')
+
+// Definición de eventos
 const emit = defineEmits(['cerrar-ventana', 'guardar-gasto', 'update:nombre', 'update:cantidad', 'update:categoria'])
 
+// Definición de propiedades
 const props = defineProps({
     nombre: {
         type: String,
@@ -20,15 +26,16 @@ const props = defineProps({
         type: String,
         required: true
     },
-    disponible:
-    {
+    disponible: {
         type: Number,
         required: true
     }
 })
 
+// Función para agregar un gasto
 const aggGasto = () => {
     const { nombre, cantidad, categoria, disponible } = props
+    // Verifica si todos los campos están llenos
     if ([nombre, cantidad, categoria].includes('')) {
         error.value = "Todos los Campos son Obligatorios"
 
@@ -36,6 +43,7 @@ const aggGasto = () => {
             error.value = ''
         }, 3000)
     }
+    // Verifica si la cantidad es mayor que 0
     else if (cantidad <= 0) {
         error.value = "El gasto debe ser mayor a 0"
 
@@ -43,42 +51,52 @@ const aggGasto = () => {
             error.value = ''
         }, 3000)
     }
+    // Verifica si la cantidad no excede el disponible
     else if (cantidad > disponible) {
         error.value = "El gasto no puede ser mayor al disponible"
+
+        setTimeout(() => {
+            error.value = ''
+        }, 3000)
     }
+    // Si todas las validaciones pasan, emite el evento para guardar el gasto
     else {
         emit('guardar-gasto')
     }
 }
-
-
 </script>
 
 <template>
     <section class="ventana-emergente">
         <div class="cerrar-Ventana">
+            <!-- Botón de cierre de la ventana -->
             <img :src="iconoCerrar" @click="emit('cerrar-ventana')">
         </div>
 
         <div class="contenedor">
+            <!-- Formulario para agregar un gasto -->
             <form class="contenedor-formulario" @submit.prevent="aggGasto">
 
                 <legend>Agregar Gasto</legend>
 
+                <!-- Componente para mostrar mensajes de error -->
                 <global_error v-if="error">{{ error }}</global_error>
 
+                <!-- Campo para el nombre del gasto -->
                 <div class="Campo">
                     <label for="Nombre">Nombre Gasto: </label>
                     <input id="nombreGasto" type="text" placeholder="Agrega el nombre del gasto" :value="props.nombre"
                         @input="emit('update:nombre', $event.target.value)">
                 </div>
 
+                <!-- Campo para el monto del gasto -->
                 <div class="Campo">
                     <label for="cantidad">Monto del Gasto: </label>
                     <input id="cantidadGasto" type="number" placeholder="Agrega el monto del gasto"
                         :value="props.cantidad" @input="emit('update:cantidad', +$event.target.value)">
                 </div>
 
+                <!-- Campo para la categoría del gasto -->
                 <div class="Campo">
                     <label for="categoria">Categoria: </label>
                     <select id="categoria" :value="props.categoria"
@@ -94,6 +112,7 @@ const aggGasto = () => {
                     </select>
                 </div>
 
+                <!-- Botón para agregar el gasto -->
                 <input type="submit" value="Agregar">
 
             </form>
@@ -102,6 +121,7 @@ const aggGasto = () => {
 </template>
 
 <style scoped>
+/* Estilo para la ventana emergente */
 .ventana-emergente {
     position: absolute;
     background-color: rgb(0 0 0 / 0.9);
@@ -111,23 +131,27 @@ const aggGasto = () => {
     bottom: 0;
 }
 
+/* Estilo para el botón de cierre */
 .cerrar-Ventana {
     position: absolute;
     right: 3rem;
     top: 3rem;
 }
 
+/* Estilo para la imagen del botón de cierre */
 .cerrar-Ventana img {
     width: 3rem;
     cursor: pointer;
 }
 
+/* Estilo para el formulario */
 .contenedor-formulario {
     margin: 10rem auto 0 auto;
     display: grid;
     gap: 2rem;
 }
 
+/* Estilo para el título del formulario */
 .contenedor-formulario legend {
     text-align: center;
     color: var(--GrisAzul-50);
@@ -135,6 +159,7 @@ const aggGasto = () => {
     font-weight: 700;
 }
 
+/* Estilo para entradas y selects en el formulario */
 .contenedor-formulario input,
 .contenedor-formulario select {
     background-color: var(--GrisAzul-200);
@@ -143,10 +168,12 @@ const aggGasto = () => {
     border: none;
 }
 
+/* Estilo para etiquetas en el formulario */
 .contenedor-formulario label {
     color: var(--GrisAzul-50)
 }
 
+/* Estilo para el botón de envío */
 .contenedor-formulario input[type="submit"] {
     background-color: var(--GrisAzul-700);
     color: var(--GrisAzul-50);
@@ -154,6 +181,7 @@ const aggGasto = () => {
     font-weight: 700;
 }
 
+/* Estilo para cada campo del formulario */
 .Campo {
     display: grid;
     gap: 2rem;
